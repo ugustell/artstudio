659554ae0c004d4861d72649be122830a6c80532
ugustell <ekaterinaprokopova907@gmail.com>
2026-03-26 11:48:27 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,18 @@ const allowedOrigins = [
   'http://localhost:3000',
 ];
 app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // для Postman / curl
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('CORS not allowed'));
+    }
+  },
+  credentials: true
+}));
+app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin) || /\.vercel\.app$/.test(origin))
